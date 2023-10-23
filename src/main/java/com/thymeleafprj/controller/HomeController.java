5cdc54024d2d636c456d3cb29098825892b0b4e4
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-10-23 18:25:29 +0530

    adding static files like css, js, and images in thymeleaf

--- src/main/java/com/thymeleafprj/controller/HomeController.java
+++ src/main/java/com/thymeleafprj/controller/HomeController.java
@@ -1,4 +1,5 @@
 package com.thymeleafprj.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,16 +11,6 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("title","Home");
 		return "index";
-	}
-	@RequestMapping("/about")
-	public String about(Model model) {
-		model.addAttribute("title","About");
-		return "about";
-	}
-	@RequestMapping("/service")
-	public String service(Model model) {
-		model.addAttribute("title","Service");
-		return "service";
 	}
 	
 }
